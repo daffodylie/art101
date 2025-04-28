d85abdc1dec9c8df7948a577803d86942b970d14
daffodylie <daffodyl15@gmail.com>
2025-04-28 14:49:25 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 6 index.js - Arrays and Objects: 
+  // Create an array with modes of transportation, an element with your primary mode, and a function that calculates age of vehicle.
+// Author: Dylan Boyle <dmboyle@ucsc.edu>
+// Date: April 28, 2025
 
-// Constants
+//Define Vehicle
+myTransport = [" Hyundai Sonata", " bus", " rides from friends"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//Create object for my car
+myMainRide = {
+  make: "Hyundai",
+  model: "Sonata",
+  color: "blue",
+  year: 2016,
+  //Define a function that calculates age of my car
+  age : function() {
+    return 2025 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
