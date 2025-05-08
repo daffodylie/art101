d6e2e2b205cd3fb2f6d6a0e94d5868f3f7022c29
daffodylie <daffodyl15@gmail.com>
2025-05-08 15:38:33 -0700

    debugpt3

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -2,30 +2,30 @@
 // Author: Dylan Boyle <dmboyle@ucsc.edu>
 // Date: May 8, 2025
 
-    //function that adds five
-    function plusFive(x) {
-        return x + 5;
-    }
+//function that adds five
+function plusFive(x) {
+    return x + 5;
+}
 
-    //testing function
-    console.log(plusFive(10));
-    console.log(plusFive(15));
-    console.log(plusFive(20));
+//testing function
+console.log(plusFive(10));
+console.log(plusFive(15));
+console.log(plusFive(20));
 
-    //defining array
-    numArray = [15, 16, 17, 18]
-    console.log("My Array: ", numArray);
+//defining array
+numArray = [15, 16, 17, 18]
+console.log("My Array: ", numArray);
 
-    //mapping the array
-    let results = numArray.map(plusFive);
+//mapping the array
+let results = numArray.map(plusFive);
 
-    //expected results: 20, 21, 22, 23
-    console.log("Results: ", results);
+//expected results: 20, 21, 22, 23
+console.log("Results: ", results);
 
-    //anon function that multiplies by 5
-    let result = numArray.map(function(x){
-        return x * 5
-    })
+//anon function that multiplies by 5
+let multiplied = numArray.map(function(x){
+    return x * 5
+})
 
-    //expected results: 75, 80, 85, 90
-    console.log("Results: ", result);
+//expected results: 75, 80, 85, 90
+console.log("Results: ", multiplied);
