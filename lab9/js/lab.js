d131f7536b57ba43cde910f9079fbc1862eea53e
daffodylie <daffodyl15@gmail.com>
2025-05-12 21:06:11 -0700

    taskx

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,28 +3,37 @@
 // Date: May 12, 2025
 
 // add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+//$("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
 // add a click listener to the challenge button
-$("#button-challenge").click(function(){
+//$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
-});
+    //$("#challenge").toggleClass("special");
+//});
 
 // add button to problems section
-$("#problems").append("<button id='button-problems'>Make Special</button>");
+//$("#problems").append("<button id='button-problems'>Make Special</button>");
 
 // add a click listener to the problems button
-$("#button-problems").click(function(){
+//$("#button-problems").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#problems").toggleClass("special");
-});
+    //$("#problems").toggleClass("special");
+//});
 
 // add button to results section
-$("#results").append("<button id='button-results'>Make Special</button>");
+//$("#results").append("<button id='button-results'>Make Special</button>");
 
 // add a click listener to the results button
-$("#button-results").click(function(){
+//$("#button-results").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#results").toggleClass("special");
+    //$("#results").toggleClass("special");
+//});
+
+// add button to results section
+$(".minor-section").append("<button id='special-button'>Make Special</button>");
+
+// add a click listener to the results button
+$("#special-button").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $(this).parent().toggleClass("special");
 });
